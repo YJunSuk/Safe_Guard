d8a72fd38cf6799c3e57c1623ac1bb6482a826d8
JS-Yang <junsuk530@gmail.com>
2023-12-09 17:57:30 +0900

    fireabse, storage 연결

---
+++ flutter/safe_guard/lib/home.dart
@@ -0,0 +1,129 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:safe_guard/firebase_options.dart';
+import 'package:safe_guard/camera.dart';
+
+
+class Home extends StatefulWidget {
+
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  // Firestore에서 'name' 필드를 가져오는 함수
+  Future<String> getTitleName(String docid) async {
+    var document = await firestore.collection('test').doc(docid).get();
+    if (document.exists) {
+      return document.data()!['name'] ?? 'No Title';
+    } else {
+      return 'No Title';
+    }
+  }
+
+  // Firestore에서 이미지 URL을 가져오는 함수
+  Future<String> getImageUrl(String docid) async {
+    var document = await firestore.collection('test').doc(docid).get();
+    if (document.exists) {
+      return document.data()!['image'] ?? ''; // 'image' 필드에 URL이 있다고 가정
+    } else {
+      return ''; // 문서가 존재하지 않을 경우 빈 문자열 반환
+    }
+  }
+
+  Widget title(String listnum){
+    return FutureBuilder<String>(
+          future: getTitleName(listnum),
+          builder: (context, snapshot){
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return Text('Loading...');
+            }
+            return Text(snapshot.data ?? 'No Title');
+          }
+          );
+  }
+
+  Widget image(String listnum){
+    return FutureBuilder<String>(
+        future: getImageUrl(listnum),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator(); // 로딩 중
+          }
+          if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+            return Center(
+              child: Image.network(snapshot.data!), // 이미지 표시
+            );
+          } else {
+            return const Center(
+              child: Text('이미지를 불러올 수 없습니다.'),
+            );
+          }
+        },
+      );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Safe_Guard'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.camera_indoor,
+                size: 50
+                ),
+              tooltip: 'CCTV 전환',
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context){
+                  return Camera();
+                },
+                ));
+              },
+              )
+          ],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestore.collection('test').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+          if (!snapshot.hasData) {
+            return Text('No data available');
+          }
+
+          List<DocumentSnapshot> docs = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic> data = docs[index].data() as Map<String, dynamic>;
+              String name = data['name'] ?? 'No Title';
+              String imageUrl = data['image'] ?? '';
+
+              return Container(
+                height: 150,
+                child: Row(
+                  children: [
+                    Expanded(child: Text(name)),
+                    imageUrl.isNotEmpty ? Expanded(child: Image.network(imageUrl)) : Container(),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- flutter/safe_guard/lib/main.dart
+++ flutter/safe_guard/lib/main.dart
@@ -1,72 +1,28 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:safe_guard/firebase_options.dart';
+import 'package:safe_guard/home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
-  
-  Future<String> readData() async {
-    List<String> list = [];
-    CollectionReference collection = firestore.collection('test');
+  const MyApp({super.key});
 
-    // 컬렉션의 모든 문서 가져오기
-    QuerySnapshot querySnapshot = await collection.get();
-
-    // 각 문서의 데이터 출력
-    for (var doc in querySnapshot.docs) {
-      list.add(doc.data().toString());
-    }
-    return list.isNotEmpty ? list.first : "No Data Found";
-  }
-
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: FutureBuilder<String>(
-        future: readData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          }
-          return MyHomePage(title: snapshot.data ?? 'Flutter Demo Home Page');
-        },
-      ),
+      title: 'Safe_Guard',
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
 
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text('Your content here'),
-      ),
-    );
-  }
-}
